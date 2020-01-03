a6cadaaaff9df0eb164f603b3699968a957d619e
John O'Sullivan <johnosull9@hotmail.com>
2020-01-03 15:44:50 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sylph',
+    description='A lightweight python test automation library',
+
+    version='0.0.1',
+    author="John O'Sullivan",
+    author_email='johnosull9@hotmail.com',
+    url='https://www.linkedin.com/in/johnosull9/',
+
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+)
