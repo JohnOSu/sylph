2c6a03d32c06e1425c584fbf104eacf072bf0d75
JohnOSu <johnosull9@hotmail.com>
2020-11-24 09:51:34 +0000

    6.1 v update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='sylph',
     description='A lightweight python test automation library',
 
-    version='0.6.0',
+    version='0.6.1',
     author="John O'Sullivan",
     author_email='johnosull9@hotmail.com',
     url='https://www.linkedin.com/in/johnosull9/',
