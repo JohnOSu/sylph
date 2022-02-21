4a7b0502d3c6784e4a0a434a69ea4dd9c827a224
John O'Sullivan <josullivan@letsgetchecked.com>
2022-02-21 08:23:35 +0000

    Refactored is_element_available

--- src/sylph_proj/pages.py
+++ src/sylph_proj/pages.py
@@ -30,12 +30,20 @@ class BasePage(metaclass=ABCMeta):
 
     def _is_done_loading(self, locator_elem) -> bool:
         self.log.info(f'{self.page_name} is loading...')
-        is_ready = self.is_element_available(locator_elem, name=self.page_name)
+        is_ready = self.is_element_available(locator_elem)
         if is_ready:
             self.log.info(f'{self.page_name} is available')
         return is_ready
 
-    def is_element_available(self, elem, wait=30, name=None) -> bool:
+    def _get_elem_name(self, locator):
+        try:
+            page_name = locator.__closure__[0].cell_contents.page_name
+            element = locator.__code__.co_names[0]
+            return f'{page_name}.{element}'
+        except:
+            return None
+
+    def is_element_available(self, elem, wait=30) -> bool:
         """Repeated safe check for the specified wait time (seconds) until the element is displayed and enabled.
            If not found, return false.
 
@@ -48,6 +56,9 @@ class BasePage(metaclass=ABCMeta):
             True if element is displayed.
 
         """
+        name = self._get_elem_name(elem)
+        if not name:
+            name = self.page_name
         e = None
         beginning = time.time()
         for w in range(0, wait):
@@ -64,19 +75,20 @@ class BasePage(metaclass=ABCMeta):
             time.sleep(1)
             since = time.time()
             span = self.span(since, beginning)
-            
+
             span_msg = f'Elapsed seconds: {span}'
-            wait_msg = f'Waiting for {name}'
-            wait_msg = f'{wait_msg}: {name} | {span_msg}' if name else f'{wait_msg}. | {span_msg}'
-            self.log.debug(wait_msg)
+            wait_msg = f'Waiting for: {name} | {span_msg}'
+            self.log.info(wait_msg)
 
             if span >= wait:
-                wait_msg = f'{name} not found' if name else 'Element not found'
-                self.log.debug(wait_msg)
+                wait_msg = f'{name} not found'
+                self.log.info(wait_msg)
                 return False
 
-        msg = 'Found Element'
-        self.log.debug(f'{msg}: {e.id}' if e else msg)
+        msg = f'Found {name} at {e.location}\nLoc in view: {e.location_in_view}\n' \
+              f'If scrolled: {e.location_once_scrolled_into_view}\n' \
+              f'       Rect: {e.rect}'
+        self.log.info(msg)
         return True
 
     def wait_for_condition(self, condition, wait=10):
@@ -150,54 +162,27 @@ class BasePageMobile(BasePage):
         if with_validation and not self._is_done_loading(locator_elem):
             raise Exception("App.PAGE_LOAD")
 
-    def try_find_element(self, locator, max_swipes=6, swipe_dir=BasePage.SWIPE_UP, name=None):
-        """Repeated swipe action (default:up) for the specified number of attempts or until the element is found.
-           If not found, no consequences.
-
-        Args:
-            :param locator: A lambda function that returns a webelement.
-            :param max_swipes: The max number of swipes to attempt
-            :param swipe_dir: 'up' to reveal elements below, 'down' to reveal elements above
-            :param name: (Optional) A name describing the webelement for logging purposes.
+    def try_find_element(self, **kwargs):
         """
-        located = self.is_element_available(lambda: locator(), 2, name)
+        :param kwargs: locator - A lambda for a property that returns a WebElement
+        :param kwargs: swipe_action - A tuple containing coordinates and a duration
+
+        Usage::\n
+        locator (always required) should be a lambda for a property that returns a WebElement. swipe_action should
+        be a tuple of coordinates and a duration - e.g:
+            locator=lambda: self._start_btn, swipe_action=(1000, 800, 40, 800, 500)
+        """
+        locator = kwargs['locator']
+        action = kwargs['swipe_action']
+        max_swipes = kwargs['max_swipes'] if 'max_swipes' in kwargs else 6
+
+        located = self.is_element_available(lambda: locator(), 2)
         attempts = 0
         while not located:
-            attempts +=1
-            self.log.info(f'Swiping: {swipe_dir}')
-            if swipe_dir is BasePage.SWIPE_UP:
-                self.swipe_up()
-            elif swipe_dir is BasePage.SWIPE_DOWN:
-                self.swipe_down()
-            elif swipe_dir is BasePage.SWIPE_LEFT:
-                self.swipe_left()
-            else:
-                self.swipe_right()
+            attempts += 1
+            self.log.info(f'Swiping...')
+            self.driver.swipe(*action)
 
-            located = self.is_element_available(lambda: locator(), 2, name)
+            located = self.is_element_available(lambda: locator(), 2)
             if attempts >= max_swipes:
                 break
-
-    def swipe_up(self):
-        if self.config.is_ios:
-            self.driver.swipe(50, 350, 50, 310, 400)
-        else:
-            self.driver.swipe(100, 1000, 100, 845, 400)
-
-    def swipe_down(self):
-        if self.config.is_ios:
-            self.driver.swipe(50, 310, 50, 350, 400)
-        else:
-            self.driver.swipe(100, 845, 100, 1000, 400)
-
-    def swipe_left(self):
-        if self.config.is_ios:
-            self.driver.swipe(300, 250, 80, 250, 400)
-        else:
-            self.driver.swipe(600, 800, 500, 800, 400)
-
-    def swipe_right(self):
-        if self.config.is_ios:
-            self.driver.swipe(80, 250, 300, 250, 400)
-        else:
-            self.driver.swipe(500, 800, 600, 800, 400)
