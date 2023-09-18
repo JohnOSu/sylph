144a3cdddcacc3af8df3e88d02bd5d54233ee8ec
jcarvalho <jcarvalho@letsgetchecked.com>
2023-09-18 16:54:50 +0100

    bump version 0.10.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='sylph',
     description='A lightweight python test automation library',
 
-    version='0.10.5',
+    version='0.10.6',
     author="John O'Sullivan",
     author_email='johnosull9@hotmail.com',
     url='https://www.linkedin.com/in/johnosull9/',
